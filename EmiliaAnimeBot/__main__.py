391836152e54892e68e178c9b36559609386d387
Nine <89542501+Ninematsuno@users.noreply.github.com>
2021-10-02 12:06:42 +0530

    Update __main__.py

--- EmiliaAnimeBot/__main__.py
+++ EmiliaAnimeBot/__main__.py
@@ -92,7 +92,7 @@ buttons = [
 
 
 HELP_STRINGS = f"""
-`Hey there! My name is` [{BOT_NAME}]({EMILIA_HELP_IMG}) 
+`Hey there! My name is` [{NAMIKAZE_MINATO}]({https://telegra.ph/file/c64faeb5ca0f9885c8ab1.jpg}) 
 I have Quite a Few Features, Go Ahead and Check out!"""
 
 IMPORTED = {}
