6b90a5fd12f18aeb905b8f06618ab195d636f366
Nine <89542501+Ninematsuno@users.noreply.github.com>
2021-10-03 13:18:50 +0530

    Update emiranks.py

--- EmiliaAnimeBot/modules/emiranks.py
+++ EmiliaAnimeBot/modules/emiranks.py
@@ -480,7 +480,7 @@ def warcommanders(update: Update, context: CallbackContext):
 @whitelist_plus
 def supremedemons(update: Update, context: CallbackContext):
     bot = context.bot
-    reply = "<b>Jounin:</b>\n"
+    reply = "<b>Jouchin:</b>\n"
     for each_user in DEMONS:
         user_id = int(each_user)
         try:
