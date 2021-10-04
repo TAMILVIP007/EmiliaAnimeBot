232f8135792561ac1c59ebd8cb9d9e86046ead16
Nine <89542501+Ninematsuno@users.noreply.github.com>
2021-10-04 19:57:29 +0530

    Update __main__.py

--- EmiliaAnimeBot/__main__.py
+++ EmiliaAnimeBot/__main__.py
@@ -588,7 +588,7 @@ def main():
 
     if SUPPORT_CHAT is not None and isinstance(SUPPORT_CHAT, str):
         try:
-            dispatcher.bot.sendMessage(f"@{SUPPORT_CHAT}", "[is Back Online 💼](https://telegra.ph/file/9edd62e985fa62b63aae2.jpg)")
+            dispatcher.bot.sendMessage("https://t.me/minato_support", "[Minato is back online 🈲](https://telegra.ph/file/9edd62e985fa62b63aae2.jpg)")
         except Unauthorized:
             LOGGER.warning(
                 "Bot isnt able to send message to SUPPORT_CHAT, go and check!"
